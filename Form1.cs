702607871b10a558dbcec43c3dd87d12ec40bfcb
Rami Sihvo <rami.sihvo@pp.inet.fi>
2024-03-19 15:24:14 +0200

    First working game commit
    Can move around and read text

---
+++ Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DungeonOfDoom
+{
+   
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+        InitializeComponent();
+        xyLabel.Text = Convert.ToString(Program.locX) + "," + Convert.ToString(Program.locY);
+        checkWalls();
+        updateRoom();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textField_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void actionItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void goNorth_Click(object sender, EventArgs e)
+        {
+             
+            Program.locY--;
+            checkWalls();
+            updateRoom();
+        }
+
+        private void goEast_Click(object sender, EventArgs e)
+        {
+            Program.locX++;
+            checkWalls();
+            updateRoom();
+        }
+           
+
+        private void goSouth_Click(object sender, EventArgs e)
+        {
+            Program.locY++;
+            checkWalls();
+            updateRoom();
+
+        }
+
+        private void goWest_Click(object sender, EventArgs e)
+        {
+            Program.locX--;
+            checkWalls();
+            updateRoom();
+        }
+
+        public void updateRoom()
+        {
+            //tulostetaan tekstikenttään huoneen kuvaus
+            textField.Text = Program.caveInfo[Program.locY, Program.locX, 0];
+            //Tulostetaan kuva
+
+            //merkataan harmaalla jos napille ei ole toimintoa (null array)
+        }
+        public void checkWalls()
+        {
+           
+            if (Program.locArray[Program.locY-1, Program.locX] > 0)
+            {
+                goNorth.Enabled = false;
+            }
+            else
+            {
+                goNorth.Enabled = true;
+            }
+
+            if (Program.locArray[Program.locY + 1, Program.locX] > 0)
+            {
+                goSouth.Enabled = false;
+            }
+            else
+            {
+                goSouth.Enabled = true;
+            }
+
+            if (Program.locArray[Program.locY, Program.locX - 1] > 0)
+            {
+                goWest.Enabled = false;
+            }
+            else
+            {
+                goWest.Enabled = true;
+            }
+
+            if (Program.locArray[Program.locY, Program.locX + 1] > 0)
+            {
+                goEast.Enabled = false;
+            }
+            else
+            {
+                goEast.Enabled = true;
+            }
+            xyLabel.Text = Convert.ToString(Program.locX) + "," + Convert.ToString(Program.locY);
+
+        }
+
+
+
+
+        private void xyLabel_Click(object sender, EventArgs e)
+        {
+           // xyLabel.Text = Convert.ToString(Program.locX) + "," + Convert.ToString(Program.locY);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DungeonOfDoom
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        //[STAThread]
+
+        //Luodaan kaksiulotteinen array. Tämä on luolasto
+        //0 = tyhjä huone, 1 = avaimen numero joka pitää olla että huoneeseen voi mennä
+        //ok ehkä 1 = kiviseinä josta ei pääse läpi
+
+        //locArray[y,x]
+        //vaakarivi y, pystyrivi x 
+
+        public static int[,] locArray = {
+            {0 ,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20},
+            {1 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {2 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {3 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {4 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {5 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {6 ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {7 ,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1},
+            {8 ,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1},
+            {9 ,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1},
+            {10,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1},
+            {11,1,1,1,1,1,1,1,1,1,0,3,0,0,1,1,1,1,1,1,1},
+            {12,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1},
+            {13,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1},
+            {14,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1},
+            {15,1,1,1,1,1,1,1,0,1,0,1,0,1,1,1,1,1,1,1,1},
+            {16,1,1,1,1,1,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1},
+            {17,1,1,1,1,1,1,1,0,1,0,1,0,1,1,1,1,1,1,1,1},
+            {18,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1},
+            {19,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1},
+            {20,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+            };
+
+        //paikkamme alussa (locArray)
+        public static int locX = 10;
+        public static int locY = 19;
+
+        //start x 10 y 19
+        //intro screens&text x10,y18  ja x10,y17
+        //eka varsinainen huone x10, y16
+
+        public static bool key2 = false;
+        public static bool key3 = false;
+
+        public static string[,,] caveInfo = new string[21, 21, 21];
+
+        //luodaan kolmiulotteinen array tiedoille, tämä jää alussa täyttämättä
+        //info array on täytetty tiedoilla, x,y vastaavat locArrayn paikkaa, mutta z kertoo eri vaihtoehtoja 
+        //nämä kaksi arrayta voisivat olla samat, mutta halusimme alustaa arrayn graafisesti.
+
+   
+        static void Main()
+        {
+            SetInfo();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        }
+
+        public static void SetInfo()
+        {
+            caveInfo[19, 10, 0] = "Welcome to the Dungeon of Doom! Developed by Rami Sihvo, Finnish College Jam Game 2024. (Press North to start your adventure)";
+            caveInfo[18, 10, 0] = "Daring lady doctor Croft has been lost to ancient Aztek Temple and it is up to you to find her. Here are stairs winding down to bowels of the earth.";
+            caveInfo[17, 10, 0] = "Damp, ancient ruins. Probably some 2000 years old. About the age of my history teacher, that is.";
+            caveInfo[16, 10, 0] = "You came across to ancient hall of previously unknown Aztek temple. Doctor Croft wasn't sure even what heathen god did they worship.";
+            caveInfo[15, 10, 0] = "Shrine of the ancient aztek god with stone slab under it. The slab resembles doorway.";
+
+        }
+
+
+    }
+}
